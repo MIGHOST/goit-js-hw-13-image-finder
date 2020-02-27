e1a76edabb12133d592f9f86ac9abe0fc9f27c5f
Migho <mi.sidorenko@gmail.com>
2020-02-27 22:55:51 +0200

    added_DOM_render

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './styles.css';
 import imgItems from "../src/templates/pictures.hbs";
 import "./js/imgServices";
+
+import Form from "./js/imgServices.js";
+const bodyDOM = document.querySelector("body");
+
+const searchForm = new Form();
+console.log(searchForm);
+searchForm.start(bodyDOM);  
--- src/js/imgServices.js
+++ src/js/imgServices.js
@@ -1,56 +1,100 @@
 import newsService from "./services/getAPI";
 import picturesTemplate from "../templates/pictures.hbs";
 
+class Form {
+  constructor() {
+    this.body = document.querySelector("body");
+
+    this.searchForm = null;
+    this.articleList = null;
+    this.searchInput = null;
+    this.itemsImg = null;
+    this.loadMoreBtn = null
+  }
 
 
-const refs = {
-  searchForm: document.querySelector("#search-form"),
-  searchInput: document.querySelector(".js-search-input"),
-  articleList: document.querySelector("#article-list"),
-  loadMoreBtn: document.querySelector('button[data-action="load-more"]')
-};
-refs.searchForm.addEventListener("submit", searchImg);
-// refs.loadMoreBtn.addEventListener("click", loadMoreBtnHandler)
+  makeSearchForm() {
+    return `
+    <form class="search-form" id="search-form">
+    <input
+      type="text"
+      name="query"
+      autocomplete="off"
+      class = "js-search-input search-input"
+      placeholder="Search images..."
+    />
+  </form>`
+  }
+  makeImgList() {
+    return `<ul class="gallery js-gallery__item">
+</ul>
+  `
+  }
+  makeBtn() {
+    return `<button type="button" class = "js-btn" data-action="load-more">Load more</button>`
+  }
 
-function searchImg(e){
-  e.preventDefault();
+  start(container) {
+    this.addToScreen(container, "beforeend", this.makeSearchForm());
+    this.addToScreen(container, "beforeend", this.makeImgList());
+    this.searchForm = document.querySelector("#search-form");
+    this.searchInput = document.querySelector(".js-search-input");
+    this.articleList = document.querySelector(".js-gallery__item");    
+    this.searchForm.addEventListener("submit", this.searchImg.bind(this));
+ 
+
+
+  }
+  addToScreen(container, position, el) {
+    container.insertAdjacentHTML(position, el)
+  }
+
+  searchImg(e) {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const input = form.elements.query;
+    this.clearListItems();
+    newsService.resetPage();
+    newsService.searchQuery = input.value;
   
-  const form = e.currentTarget;
-  const input = form.elements.query;
+    this.fetchArticles();
+    this.renderBtn()
+    this.btn = document.querySelector(".js-btn")
+    this.btn.addEventListener("click", this.loadMoreBtnHandler())
+    input.value = '';
 
-  clearListItems()
-  newsService.resetPage();
-  newsService.searchQuery = input.value;
-  fetchArticles();
 
-  input.value = '';
+  }
+  fetchArticles() {
+
+    newsService.fetchArticle()
+      .then(data => {
+        this.build(data);
+      })
+      .catch(error => {
+        console.warn(error);
+      });
+  }
+
+  build(items) {
+    const markup = items.reduce((acc, el) => picturesTemplate(el) + acc, "");
+
+    this.articleList.insertAdjacentHTML("afterbegin", markup)
+  }
+  renderBtn() {
+    const list = document.querySelector(".gallery");
+    this.addToScreen(list, "beforeend", this.makeBtn())
+  }
+
+  clearListItems() {
+    this.articleList.innerHTML = '';
+  }
+
+  loadMoreBtnHandler() {
+    this.fetchArticles();
+  }
+
 }
-function fetchArticles() {
 
-
-  newsService
-    .fetchArticle()
-    .then(hits => {
-   
-     build(hits);
-    })
-    .catch(error => {
-      console.warn(error);
-    });
-}
-
-function build (items){
-const markup = picturesTemplate(items)
-  refs.articleList.insertAdjacentHTML("beforeend", markup)
-}
-
-
-function clearListItems() {
-  refs.articleList.innerHTML = '';
-}
-
-function loadMoreBtnHandler() {
-    newsService.fetchArticle().then(insertListItems);
-}
-
-
+export default Form;
